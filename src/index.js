05a95fe438ca79de2203df116622559056855645
Yuliya Khilko <ykhilko@gmail.com>
2020-10-29 16:12:12 -0700

    change basename in browser router and change homepage in package json

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-     <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <BrowserRouter>
       <App />
      </BrowserRouter>
     </React.StrictMode>,
